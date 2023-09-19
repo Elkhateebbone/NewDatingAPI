e0b068db7cb414206e862343c9b9dc379749fad6
DELL LATTITUDE <DELL LATTITUDE@DESKTOP-V4B09AO>
2023-09-19 14:38:19 -0700

    Add project files.

---
+++ Chatting/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Chatting.Data;
+using Chatting.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+
+            var users = _context.Users.ToList();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+            
+        }
+    }
+}
